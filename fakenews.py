22b7de9b251c8f6873c618307aa38c476fe23ea7
NAMRITHA <107317045+NamrithaGirish@users.noreply.github.com>
2022-07-08 19:07:43 +0530

    Create fakenews.py

---
+++ fakenews.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import joblib
+model=joblib.load('fake-news')
+st.title("FAKE SITE DETECTOR")
+ip=st.text_input("WEBSITE NAME")
+op=model.predict([ip])
+if st.button("CHECK"):
+  if op[0]==0:
+    st.title("FAKE NEWS")
+  else :
+    st.title("GENUINE NEWS")
