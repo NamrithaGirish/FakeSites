39fdec20533a149f13ce958182c2a358295df107
NAMRITHA <107317045+NamrithaGirish@users.noreply.github.com>
2022-07-08 19:39:44 +0530

    Update fakenews.py

--- fakenews.py
+++ fakenews.py
@@ -1,12 +1,14 @@
-import streamlit as st
-import joblib
-model=joblib.load('fake-news')
-st.title("FAKE SITE DETECTOR")
-ip=st.text_input("WEBSITE NAME")
-op=model.predict([ip])
-if st.button("CHECK"):
-  if op[0]==0:
-    st.title("FAKE NEWS")
-  else :
-    st.title("GENUINE NEWS")
-   
+import streamlit as st 
+import joblib 
+
+#load the joblib model 
+model_nb = joblib.load('fake-news')
+
+#user input 
+st.title("FAKE SITE DETECTION")
+ip = st.text_input("WEBSITE URL/NAME")
+
+#predict if the entered message is spam or ham 
+op = model_nb.predict([ip])
+if st.button('PREDICT'):
+  st.title(op[0])  #prints the output as spam or ham
