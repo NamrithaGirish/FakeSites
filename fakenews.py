cdcf467be439cc33b707c16d6ae5ee0d957f015c
NAMRITHA <107317045+NamrithaGirish@users.noreply.github.com>
2022-07-09 18:29:00 +0530

    Update fakenews.py

--- fakenews.py
+++ fakenews.py
@@ -1,14 +1,14 @@
 import streamlit as st 
 import joblib 
 
-#load the joblib model 
+#LOADING JOBLIB MODEL 
 model = joblib.load('fake-news')
 
 #user input 
 st.title("FAKE NEWS WEBSITE DETECTOR")
 ip = st.text_input("ENTER WEBSITE URL/ NAME")
 
-#modifying input
+#MODIFYING INPUT
 inpt =''
 for i in ip:
   if i.isspace():
@@ -16,10 +16,10 @@ for i in ip:
   else:
     inpt+=i
 
-#predict legitimacy of website 
+#PREDIVTING LEGITIMACY OF SITE 
 op = model.predict([inpt])
 if st.button('CHECK'):
   if op[0]==0:
-    st.header("FAKE WEBSITE")
+    st.header("FAKE NEWS WEBSITE")
   else:
-    st.header("LEGITIMATE WEBSITE")  
+    st.header("LEGITIMATE NEWS WEBSITE")  
